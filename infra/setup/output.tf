f511f6e5f1dbb5ddb09665cfff9ea878b16983e7
Vipul Kumar <vipulbishnoi88@gmail.com>
2026-02-02 16:16:32 +0530

    github workflows

--- infra/setup/output.tf
+++ infra/setup/output.tf
@@ -1,10 +1,20 @@
 output "cd_user_access_key_id" {
   value       = aws_iam_access_key.cd.id
   description = "Access Key ID for the CD IAM User"
-  
+
 }
 output "cd_user_access_key_secret" {
   value       = aws_iam_access_key.cd.secret
   description = "Secret Access Key for the CD IAM User"
-  sensitive = true
+  sensitive   = true
+}
+output "ecr_repo_app" {
+  value       = aws_ecr_repository.app.repository_url
+  description = "ECR Repository url for Application image"
+
+}
+output "ecr_repo_proxy" {
+  value       = aws_ecr_repository.proxy.repository_url
+  description = "ECR Repository url for proxy image"
+
 }
